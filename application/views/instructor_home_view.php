071c7240223de7669d7d1de1702d3faf1f277414
Nazmul Haque <nlnazmul@gmail.com>
2016-02-29 09:59:16 +0600

    instructor layout

--- application/views/instructor_home_view.php
+++ application/views/instructor_home_view.php
@@ -9,8 +9,8 @@ $full_name = $CI->session->userdata('full_name');
 
 <div class="container paddingT75">
     <div class="row">
-        <div class="col-md-12 col-ms-12 col-xs-12">
-            <h4>Feedback Course List:</h4>
+        <div class="col-md-6 col-ms-12 col-xs-12">
+            <h4>Course Feedback List:</h4>
             <br>
             <div class="row">
                 <div class="col-md-12 col-ms-12 col-xs-12">
@@ -22,9 +22,6 @@ $full_name = $CI->session->userdata('full_name');
                             <th>Course Summary</th>
                             <th width="80">Course Feedback Given List</th>
                             <th width="110">Course Feedback Not Given List</th>
-                            <th>Instructor Summary</th>
-                            <th width="80">Instructor Feedback Given List</th>
-                            <th width="110">Instructor Feedback Not Given List</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -41,7 +38,39 @@ $full_name = $CI->session->userdata('full_name');
                                 <td>
                                     <?php echo '<a href="'.base_url().'instructor_home/course_feedback_given_list/'.$my_courses[$i]->course_id.'/'.$my_courses[$i]->semester_id.'/0'.'">'.'Not Given List'.'</a>' ?>
                                 </td>
-                                <td><?php echo '<a href="'.base_url().'instructor_home/feedback_summery_teacher_wise/'.$username.'/'.$my_courses[$i]->course_id.'/'.$my_courses[$i]->semester_id.'"> '.$my_courses[$i]->full_name.' ('.$my_courses[$i]->teacher_id.') ('.$my_courses[$i]->instructor_given.')'.'</a>' ?></td>
+                            </tr>
+                            <?php $k++;} ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-ms-12 col-xs-12">
+            <h4>Instructor Feedback  List:</h4>
+            <br>
+            <div class="row">
+                <div class="col-md-12 col-ms-12 col-xs-12">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>SL</th>
+                            <!--<th>Semester Name</th>-->
+                            <!--<th>Course Summary</th>-->
+                            <th>Instructor Summary</th>
+                            <th width="80">Instructor Feedback Given List</th>
+                            <th width="110">Instructor Feedback Not Given List</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php $k=1; for ($i = 0; $i < count($my_courses); ++$i) {?>
+                            <tr>
+                                <td><?php echo $k;?></td>
+                                <!--<td><?php /*echo $my_courses[$i]->semester_id*/?></td>-->
+                                <!--<td>
+                                    <?php /*echo '<a href="'.base_url().'instructor_home/feedback_summery_course_wise/'.$my_courses[$i]->course_id.'/'.$my_courses[$i]->semester_id.'">'.$my_courses[$i]->course_name.' ('.$my_courses[$i]->course_id.') ('.$my_courses[$i]->course_given.')'.' </a>' */?>
+                                </td>-->
+                                <td><?php echo '<a href="'.base_url().'instructor_home/feedback_summery_teacher_wise/'.$username.'/'.$my_courses[$i]->course_id.'/'.$my_courses[$i]->semester_id.'"> '.$my_courses[$i]->course_name.' ('.$my_courses[$i]->course_id.') ('.$my_courses[$i]->instructor_given.')'.'</a>' ?></td>
                                 <td>
                                     <?php echo '<a href="'.base_url().'instructor_home/instructor_feedback_given_list/'.$my_courses[$i]->course_id.'/'.$my_courses[$i]->semester_id.'/'.$my_courses[$i]->teacher_id.'/1'.'">'.'Given List'.'</a>' ?>
                                 </td>
@@ -55,6 +84,7 @@ $full_name = $CI->session->userdata('full_name');
                 </div>
             </div>
         </div>
+
 
     </div>
 </div>
